1d740a86bad3cd337f557f623e0b043b9438f3b1
Shukuruloh <shukurxondev@gmail.com>
2026-02-06 17:16:01 +0500

    first commit

---
+++ app/[lng]/(root)/(home)/page.tsx
@@ -0,0 +1,12 @@
+function Page() {
+	return (
+		<div className='text-xl'>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
+			temporibus, aspernatur officia ab, quis laboriosam quibusdam perferendis
+			distinctio laudantium sunt architecto veniam quo! Id, recusandae. Ut vel
+			molestiae eligendi maiores!
+		</div>
+	)
+}
+
+export default Page
