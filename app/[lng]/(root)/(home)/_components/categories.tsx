6cb8c81ac53b56b5403cb1b8500e33654af00072
Shukuruloh <shukurxondev@gmail.com>
2026-02-09 23:42:17 +0500

    Updated

--- app/[lng]/(root)/(home)/_components/categories.tsx
+++ app/[lng]/(root)/(home)/_components/categories.tsx
@@ -1,12 +1,79 @@
+// import { categories } from '@/components/constants'
+// import Image from 'next/image'
+// import React from 'react'
+
+// function Categories() {
+// 	return (
+// 		<div className='mx-auto max-w-7xl py-8'>
+// 			<div className='flex flex-col gap-4'>
+// 				<h1 className='font-sora text-3xl font-bold'>Mahsulot toifalari</h1>
+// 				<div className='grid grid-cols-7 gap-10'>
+// 					{categories.map(item => (
+// 						<div key={item.route} className='flex flex-col gap-1'>
+// 							<div className='relative size-44'>
+// 								<Image
+// 									src={item.url}
+// 									alt={item.title}
+// 									fill
+// 									className='rounded-2xl object-cover'
+// 								/>
+// 							</div>
+// 							<h1>{item.title}</h1>
+// 						</div>
+// 					))}
+// 				</div>
+// 			</div>
+// 		</div>
+// 	)
+// }
+
+// export default Categories
+import { categories } from '@/components/constants'
+import { Separator } from '@/components/ui/separator'
+import Image from 'next/image'
 import React from 'react'
 
 function Categories() {
 	return (
-		<div className='mx-auto max-w-7xl py-8 '>
-			<div className='flex flex-col gap-4'>
-				<h1 className='font-sora text-3xl font-bold'>Mahsulot toifalari</h1>
+		<section className='mx-auto max-w-7xl py-8'>
+			{/* Header */}
+			<div className='mb-10 flex items-end justify-between'>
+				<h1 className='font-sora text-4xl font-bold tracking-tight text-slate-900'>
+					Mahsulot toifalari
+				</h1>
+				<span className='text-sm text-slate-500'>
+					Barcha kategoriyalarni ko‘ring
+				</span>
 			</div>
-		</div>
+
+			{/* Grid */}
+			<div className='grid grid-cols-6 gap-6 '>
+				{categories.map(item => (
+					<div
+						key={item.route}
+						className='group relative cursor-pointer overflow-hidden rounded-2xl bg-white shadow-md transition-all duration-500 hover:-translate-y-1 hover:shadow-2xl'
+					>
+						<div className='relative aspect-square w-full overflow-hidden'>
+							<Image
+								src={item.url}
+								alt={item.title}
+								fill
+								className='object-cover transition-transform duration-700 group-hover:scale-105'
+							/>
+
+							<div className='absolute inset-0 bg-gradient-to-t from-black/40 via-black/10 to-transparent opacity-0 transition-opacity duration-500 group-hover:opacity-100' />
+						</div>
+						<Separator />
+
+						<div className='p-4 text-center group-hover:bg-gray-100'>
+							<h3 className='text-sm font-medium text-slate-800 transition-opacity duration-300 '>
+								{item.title}
+							</h3>
+						</div>
+					</div>
+				))}
+			</div>
+		</section>
 	)
 }
 
--- components/constants/index.ts
+++ components/constants/index.ts
@@ -14,21 +14,63 @@ export const categories = [
 	{
 		route: 'kiryuvishmashinasi',
 		title: 'Kir yuvish mashinasi',
-		url: '/categories/kirmashina.webp',
+		url: '/categories/kirmashina.png',
+	},
+	{
+		route: 'televizor',
+		title: 'Televizorlar',
+		url: '/categories/televizor.jpg',
 	},
 	{
 		route: 'gazplita',
 		title: 'Gaz plitalar',
-		url: '/categories/gazplita.png',
+		url: '/categories/gaz.jpg',
+	},
+
+	{
+		route: 'pech',
+		title: 'Pechlar',
+		url: '/categories/pech.png',
 	},
 	{
 		route: 'muzlatgich',
 		title: 'Muzlatgichlar',
-		url: '/categories/muzlatgich.webp',
+		url: '/categories/muzlatgich.jpg',
 	},
 	{
-		route: 'muzlatgich',
+		route: 'dudburon',
+		title: 'Dudburonlar',
+		url: '/categories/vitoch.jpg',
+	},
+	{
+		route: 'pishirishpaneli',
+		title: 'Pishirish panellari',
+		url: '/categories/pishirishpanellari.webp',
+	},
+	{
+		route: 'konditsoner',
+		title: 'Konditsonerlar',
+		url: '/categories/konditsoner.png',
+	},
+
+	{
+		route: 'kuler',
+		title: 'Kulerlar',
+		url: '/categories/kuller.png',
+	},
+	{
+		route: 'changyutgich',
+		title: 'Changyutgichlar',
+		url: '/categories/changyutgich.jpg',
+	},
+	{
+		route: 'pishirishpaneli',
+		title: 'Pishirish panellari',
+		url: '/categories/pishirishpaneli.webp',
+	},
+	{
+		route: 'mikropesh',
 		title: 'Mikroto`lqinli pech',
-		url: '/categories/micropesh.webp',
+		url: '/categories/mikropech.webp',
 	},
 ]
