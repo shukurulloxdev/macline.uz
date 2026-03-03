7592f4a5504a8e5b362060b40fbd1cb898e2ac3d
Shukurulloh <shukurxondev@gmail.com>
2026-03-03 11:21:01 +0500

    Updated

--- app/[lng]/admin/(dashboard)/page.tsx
+++ app/[lng]/admin/(dashboard)/page.tsx
@@ -1,45 +1,43 @@
-'use client'
-import StatisticsCard from '@/app/[lng]/admin/(dashboard)/_components/cards/statistics-card'
-import { bestSeller, statistics } from '@/components/constants'
-import React from 'react'
-import BestSellerCard from './_components/cards/bestseller-card'
-import StatisticsRadio from './_components/statistics-radio'
+"use client";
+import StatisticsCard from "@/app/[lng]/admin/(dashboard)/_components/cards/statistics-card";
+import { bestSeller, statistics } from "@/components/constants";
+import React from "react";
+import BestSellerCard from "./_components/cards/bestseller-card";
+import StatisticsRadio from "./_components/statistics-radio";
 
 function Page() {
-	return (
-		<div className='overflow-hidden'>
-			<div className='space-y-8'>
-				<div>
-					<h1 className='text-4xl font-bold tracking-tight text-white'>
-						Sayt statistikasi
-					</h1>
-					<p className='mt-2 text-sm text-slate-400'>
-						Saytning real-vaqt nazorati paneli
-					</p>
-				</div>
-				<div className='grid grid-cols-4 gap-5'>
-					{statistics.map((card, i) => (
-						<StatisticsCard key={i} card={card} />
-					))}
-				</div>
+  return (
+    <div className="overflow-hidden">
+      <div className="space-y-8">
+        <div>
+          <h1 className="text-4xl font-bold tracking-tight text-white">
+            Sayt statistikasi
+          </h1>
+          <p className="mt-2 text-sm text-slate-400">
+            Saytning real-vaqt nazorati paneli Saytning real-vaqt nazorati
+            paneli
+          </p>
+        </div>
+        <div className="grid grid-cols-4 gap-5">
+          {statistics.map((card, i) => (
+            <StatisticsCard key={i} card={card} />
+          ))}
+        </div>
 
-				<div className='grid grid-cols-3 gap-5'>
-					<div
-						className='col-span-2 flex flex-col gap-4 rounded-3xl border
-	border-white/20 bg-white/10 p-6'
-					>
-						{bestSeller.map(item => (
-							<BestSellerCard key={item.image} product={item} />
-						))}
-					</div>
-					{/**/}
-					<div className='rounded-3xl border border-white/20 bg-white/10'>
-						<StatisticsRadio />
-					</div>
-				</div>
-			</div>
-		</div>
-	)
+        <div className="grid grid-cols-3 gap-5">
+          <div className="col-span-2 flex flex-col gap-4 rounded-3xl border border-white/20 bg-white/10 p-6">
+            {bestSeller.map((item) => (
+              <BestSellerCard key={item.image} product={item} />
+            ))}
+          </div>
+          {/**/}
+          <div className="rounded-3xl border border-white/20 bg-white/10">
+            <StatisticsRadio />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Page
+export default Page;
